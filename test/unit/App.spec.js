68c8a8dcab5782f8d244f035ad28be869a3de42d
Anderson <andersonecomp091@gmail.com>
2021-02-20 00:58:58 -0300

    setting up jest first component test

---
+++ test/unit/App.spec.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { render, mount, shallow } from "enzyme";
+import {expect} from 'chai'
+
+import App from "../../src/App";
+
+describe('App', () => {
+  it('rendering hello world', () => {
+    const wrapper = render(<App />);
+    expect(wrapper.html()).to.contain('<h1>Hello world!</h1>');
+  });
+})
