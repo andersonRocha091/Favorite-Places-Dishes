30dda80eaa4d0403c4f2d1c261e212f9aa29f52e
Anderson <andersonecomp091@gmail.com>
2021-02-27 20:54:56 -0300

    Restaurant list page and fixed missed add rstaurtant button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
+
+import RestaurantList from "./RestaurantList";
 
 const App = () => {
-  const [foo, setFoo] = useState('Bar');
   return (
     <div>
-      <h1>Hello world!</h1>
+      <RestaurantList />
     </div>
   );
 };
---
+++ src/RestaurantList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RestaurantList = () => {
+  return (
+    <div>
+      <button data-test="addRestaurantButton">Add Restaurant</button>
+    </div>
+  );
+};
+
+export default RestaurantList;
