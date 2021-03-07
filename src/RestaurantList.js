cfc55fe75406dfa1a839447ef611d9ba59d40aec
Anderson <andersonecomp091@gmail.com>
2021-03-07 20:57:31 -0300

    small improvement at empty list

--- src/RestaurantList.js
+++ src/RestaurantList.js
@@ -4,10 +4,12 @@ import { Collection, CollectionItem, Row, Col } from "react-materialize";
 const RestaurantList = ({ restaurants }) => {
   return (
     <Row>
-      <Col m={6} s={12}>
+      <Col m={8} s={12} l={12}>
         <Collection header="Restaurants">
           {restaurants.length === 0 ? (
-            <CollectionItem>(none added yet)</CollectionItem>
+            <CollectionItem style={{ color: "grey" }}>
+              (none added yet)
+            </CollectionItem>
           ) : (
             restaurants.map((restaurantName) => (
               <CollectionItem key={restaurantName}>
