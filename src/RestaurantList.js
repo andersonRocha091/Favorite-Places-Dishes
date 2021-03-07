5787109633d7e27cc17b85829717004ebd47ffa8
Anderson <andersonecomp091@gmail.com>
2021-03-07 01:54:21 -0300

    Improving displaying restaurants

--- src/RestaurantList.js
+++ src/RestaurantList.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React from "react";
 import { Collection, CollectionItem, Row, Col } from "react-materialize";
 
 const RestaurantList = ({ restaurants }) => {
   return (
     <Row>
       <Col m={6} s={12}>
-        <Collection>
-          {restaurants.map((restaurantName) => (
-            <CollectionItem key={restaurantName}>{restaurantName}</CollectionItem>
-          ))}
+        <Collection header="Restaurants">
+          {restaurants.length === 0 ? (
+            <CollectionItem>(none added yet)</CollectionItem>
+          ) : (
+            restaurants.map((restaurantName) => (
+              <CollectionItem key={restaurantName}>
+                {restaurantName}
+              </CollectionItem>
+            ))
+          )}
         </Collection>
       </Col>
     </Row>
