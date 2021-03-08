b1b5017dc8814fcc527d2bb55445c1649e93bffa
Anderson <andersonecomp091@gmail.com>
2021-03-07 21:51:28 -0300

    small reactoring making code clear on restaurant list

--- src/RestaurantList.js
+++ src/RestaurantList.js
@@ -6,21 +6,28 @@ const RestaurantList = ({ restaurants }) => {
     <Row>
       <Col m={8} s={12} l={12}>
         <Collection header="Restaurants">
-          {restaurants.length === 0 ? (
-            <CollectionItem style={{ color: "grey" }}>
-              (none added yet)
-            </CollectionItem>
-          ) : (
-            restaurants.map((restaurantName) => (
-              <CollectionItem key={restaurantName}>
-                {restaurantName}
-              </CollectionItem>
-            ))
-          )}
+          <RestaurantItens restaurants={restaurants} />
         </Collection>
       </Col>
     </Row>
   );
 };
+const RestaurantItens = ({ restaurants }) =>
+  restaurants.length === 0 ? (
+    <EmptyRestaurantItens />
+  ) : (
+    <SomeRestaurantItens restaurants={restaurants} />
+  );
+const EmptyRestaurantItens = () => (
+  <CollectionItem style={{ color: "grey" }}>(none added yet)</CollectionItem>
+);
+
+const SomeRestaurantItens = ({ restaurants }) => (
+  restaurants.map((restaurantName) => (
+    <CollectionItem key={restaurantName}>
+      {restaurantName}
+    </CollectionItem>
+  ))
+);
 
 export default RestaurantList;
